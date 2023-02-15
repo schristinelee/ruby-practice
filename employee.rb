23d081eddcadc3497bbb421869f105fc914738c4
schristinelee <christinelee1527@gmail.com>
2023-02-14 22:41:32 -0800

    store items as hashes

--- employee.rb
+++ employee.rb
@@ -6,10 +6,10 @@
 # puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
 # puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
 
-#represent employee data as a hash
+#represent employee data as a hash, using symbols
 
-employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true }
-employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
+employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
+employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
 
-puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
-puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
+puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
---
+++ store_item.rb
@@ -0,0 +1,11 @@
+car1 = { :color => "red", :price => 20000, :brand => "Honda" }
+
+car2 = { :color => "white", :price => 50000, :brand => "Tesla" }
+
+car3 = { color: "black", price: 30000, brand: "Kia" }
+
+puts "My #{car1[:brand]} car is #{car1[:color]} and it's #{car1[:price]} dollars."
+
+puts "My #{car2[:brand]} car is #{car2[:color]} and it's #{car2[:price]} dollars."
+
+puts "My #{car3[:brand]} car is #{car3[:color]} and it's #{car3[:price]} dollars."
