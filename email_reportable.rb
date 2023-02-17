b432d53a5c802571880c42a3ea8c3ed85dbf4e98
schristinelee <christinelee1527@gmail.com>
2023-02-16 23:26:56 -0800

    namespaces

--- email_reportable.rb
+++ email_reportable.rb
@@ -1,7 +1,9 @@
-module EmailReportable
-  def send_report
-    puts "sending email..."
-    #use email sending library
-    puts "email sent!"
+module Actualize
+  module EmailReportable
+    def send_report
+      puts "sending email..."
+      #use email sending library
+      puts "email sent!"
+    end
   end
 end
--- manager.rb
+++ manager.rb
@@ -1,32 +1,33 @@
 require "./employee.rb"
 require "./email_reportable.rb"
 
+module Actualize
+  class Manager < Employee
+    include EmailReportable
 
-class Manager < Employee
-  include EmailReportable
-
-  def initialize(input_options)
-    super
-    @employees = input_options[:employees]
-  end
-
-  def send_report
-    puts "sending email..."
-    #use email sending library
-    puts "email sent!"
-  end
-
-  def give_all_raises
-    puts "gonna give everyone a raise!"
-    @employees.each do |employee|
-      employee.give_annual_raise
+    def initialize(input_options)
+      super
+      @employees = input_options[:employees]
     end
-  end
 
-  def fire_all_employees
-    puts "ran out of money! sorry :("
-    @employees.each do |employee|
-      employee.active = false
+    def send_report
+      puts "sending email..."
+      #use email sending library
+      puts "email sent!"
+    end
+
+    def give_all_raises
+      puts "gonna give everyone a raise!"
+      @employees.each do |employee|
+        employee.give_annual_raise
+      end
+    end
+
+    def fire_all_employees
+      puts "ran out of money! sorry :("
+      @employees.each do |employee|
+        employee.active = false
+      end
     end
   end
 end
