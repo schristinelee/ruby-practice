7b7443746572013eb6404e09d6359f3564053545
schristinelee <christinelee1527@gmail.com>
2023-02-16 19:53:29 -0800

    uses module

--- employee.rb
+++ employee.rb
@@ -1,20 +1,10 @@
-# represents employee data as an array
-
-# employee1 = ["Majora", "Carter", 80000, true]
-# employee2 = ["Danilo", "Campos", 70000, true]
-
-# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-#represent employee data as a hash, using symbols
-
-# employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
-# employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
-
-# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# represent employees as a class instance
+module EmailReportable
+  def send_report
+    puts "sending email..."
+    #use email sending library
+    puts "email sent!"
+  end
+end
 
 class Employee
   attr_reader :first_name, :last_name, :active
@@ -42,7 +32,7 @@ employee1.print_info
 employee2.print_info
 
 class Manager < Employee
-  attr_reader :employees
+  include EmailReportable
 
   def initialize(input_options)
     super
@@ -73,18 +63,9 @@ end
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
-pp manager.employees
-manager.give_all_raises
-pp manager.employees
-manager.fire_all_employees
-pp manager.employees
 
 class Intern < Employee
-  def send_report
-    puts "sending email..."
-    #use email sending library
-    puts "email sent!"
-  end
+  include EmailReportable
 end
 
 intern = Intern.new(first_name: "Adrienne", last_name: "Lowe", salary: 50000, active: true)
