f6b0afbf1b0656ee5a07061de2ae8fb937b499a4
schristinelee <christinelee1527@gmail.com>
2023-02-16 18:49:24 -0800

    adds intern

--- employee.rb
+++ employee.rb
@@ -54,9 +54,39 @@ class Manager < Employee
     #use email sending library
     puts "email sent!"
   end
+
+  def give_all_raises
+    puts "gonna give everyone a raise!"
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    puts "ran out of money! sorry :("
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
 pp manager.employees
+manager.give_all_raises
+pp manager.employees
+manager.fire_all_employees
+pp manager.employees
+
+class Intern < Employee
+  def send_report
+    puts "sending email..."
+    #use email sending library
+    puts "email sent!"
+  end
+end
+
+intern = Intern.new(first_name: "Adrienne", last_name: "Lowe", salary: 50000, active: true)
+intern.print_info
+intern.send_report
